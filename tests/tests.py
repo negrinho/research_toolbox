ac5be5de3b0099df4c8b1490b7d1488dc8a33282
Renato Negrinho <negrinho@cs.cmu.edu>
2018-03-15 18:31:30 -0400

    Added some simple tests to be ran in with pytest.

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,7 @@
+import research_toolbox.tb_utils as tb_ut
 
-# this corresponds to writting a lot of tests for the toolbox. make sure 
-# that they are well matched and are easy to write.
+def test_zip_toggle():
+    xs = [[1, 2, 3], [2, 3, 4]]
+    out_xs = tb_ut.zip_toggle(tb_ut.zip_toggle(xs))
+    convert_to_tuple_fn = lambda ys: tuple(map(tuple, ys))
+    assert convert_to_tuple_fn(xs) == convert_to_tuple_fn(out_xs)
