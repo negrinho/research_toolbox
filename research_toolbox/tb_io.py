11388512a41b3fe335522ffe4ac4dc8741dc027a
Renato Negrinho <negrinho@cs.cmu.edu>
2018-11-02 20:37:30 -0400

    Added a slightly changed tokenization dictionary. Added a minimal function to work with JSON overlays.

--- research_toolbox/tb_io.py
+++ research_toolbox/tb_io.py
@@ -3,12 +3,14 @@ import json
 import pickle
 import research_toolbox.tb_utils as tb_ut
 
+
 def read_textfile(filepath, strip=True):
     with open(filepath, 'r') as f:
         lines = f.readlines()
         if strip:
             lines = [line.strip() for line in lines]
         return lines
+
 
 def write_textfile(filepath, lines, append=False, with_newline=True):
     mode = 'a' if append else 'w'
@@ -19,11 +21,13 @@ def write_textfile(filepath, lines, append=False, with_newline=True):
             if with_newline:
                 f.write("\n")
 
+
 def read_dictfile(filepath, sep='\t'):
     """This function is conceived for dicts with string keys."""
     lines = read_textfile(filepath)
     d = dict([line.split(sep, 1) for line in lines])
     return d
+
 
 def write_dictfile(filepath, d, sep="\t", ks=None):
     """This function is conceived for dicts with string keys. A set of keys
@@ -35,52 +39,80 @@ def write_dictfile(filepath, d, sep="\t", ks=None):
     lines = [sep.join([k, str(d[k])]) for k in ks]
     write_textfile(filepath, lines)
 
+
 def read_jsonfile(filepath):
     with open(filepath, 'r') as f:
         d = json.load(f)
         return d
+
 
 def write_jsonfile(d, filepath, sort_keys=False, compactify=False):
     with open(filepath, 'w') as f:
         indent = None if compactify else 4
         json.dump(d, f, indent=indent, sort_keys=sort_keys)
 
+
+def read_jsonfile_with_overlays(filepath):
+    d = read_jsonfile(filepath)
+    d_out = {}
+    if "_overlays_" in d:
+        for fp in d["_overlays_"]:
+            d_other = read_jsonfile_with_overlays(fp)
+            d_out.update(d_other)
+    d_out.update(d)
+    return d_out
+
+
 def json2key(d):
     return json.dumps(d, sort_keys=True)
+
 
 def json2str(d):
     return json.dumps(d)
 
+
 def str2json(s):
     return json.loads(s)
+
 
 def read_jsonlogfile(filepath):
     with open(filepath, 'r') as f:
         return [str2json(line) for line in f]
 
+
 def write_jsonlogfile(filepath, ds, append=False):
-    write_textfile(filepath, map(json2str, ds), append=append, with_newline=True)
+    write_textfile(
+        filepath, map(json2str, ds), append=append, with_newline=True)
+
 
 def read_picklefile(filepath):
     with open(filepath, 'rb') as f:
         return pickle.load(f)
 
+
 def write_picklefile(x, filepath):
     with open(filepath, 'wb') as f:
         pickle.dump(x, f)
+
 
 # NOTE: this function can use some existing functionality from python.
 def read_csvfile(filepath, sep=',', has_header=True):
     raise NotImplementedError
 
+
 # TODO: there is also probably some functionality for this.
-def write_csvfile(ds, filepath, sep=',',
-        write_header=True, abort_if_different_keys=True, sort_keys=False):
+def write_csvfile(ds,
+                  filepath,
+                  sep=',',
+                  write_header=True,
+                  abort_if_different_keys=True,
+                  sort_keys=False):
     ks = tb_ut.key_union(ds)
     if sort_keys:
         ks.sort()
 
-    assert (not abort_if_different_keys) or len(tb_ut.key_intersection(ds)) == len(ks)
+    assert (not abort_if_different_keys) or len(
+        tb_ut.key_intersection(ds)) == len(ks)
 
     lines = []
     if write_header:
@@ -90,4 +122,3 @@ def write_csvfile(ds, filepath, sep=',',
         lines.append(sep.join([str(d[k]) if k in d else '' for k in ks]))
 
     write_textfile(filepath, lines)
-
--- research_toolbox/tb_preprocessing.py
+++ research_toolbox/tb_preprocessing.py
@@ -1,9 +1,33 @@
 import numpy as np
 import re
 
+
+def get_ch2idx(with_pad=True,
+               with_bos=True,
+               with_eos=True,
+               with_unk=True,
+               with_overflow=True):
+    ch_lst = []
+    if with_unk:
+        ch_lst.append("_UNK_")
+    if with_bos:
+        ch_lst.append("_BOS_")
+    if with_eos:
+        ch_lst.append("_EOS_")
+    if with_pad:
+        ch_lst.append("_PAD_")
+    if with_overflow:
+        ch_lst.append("_OVERFLOW_")
+    ch_lst.extend(["\t", "\n"])
+    ch_lst.extend([chr(idx) for idx in range(32, 127)])
+    ch2idx = dict(zip(ch_lst, range(len(ch_lst))))
+    return ch2idx
+
+
 ### for simple NLP
 def keep_short_sentences(sentence_lst, max_length):
     return [s for s in sentence_lst if len(s) <= max_length]
+
 
 def count_tokens(sentence_lst):
     token_to_count = {}
@@ -15,28 +39,43 @@ def count_tokens(sentence_lst):
                 token_to_count[tk] = 1
     return token_to_count
 
+
 def keep_most_frequent_tokens(token_to_count, num_tokens):
-    top_items = sorted(token_to_count.items(),
-        key=lambda x: x[1], reverse=True)[:num_tokens]
+    top_items = sorted(
+        token_to_count.items(), key=lambda x: x[1], reverse=True)[:num_tokens]
     return dict(top_items)
 
+
 def remove_rare_tokens(token_to_count, keep_thres):
-    return {tk : c for (tk, c) in token_to_count.iteritems() if c >= keep_thres}
+    return {tk: c for (tk, c) in token_to_count.iteritems() if c >= keep_thres}
+
 
 def index_tokens(tokens, start_index=0):
     assert start_index >= 0
     num_tokens = len(tokens)
-    token_to_index = dict(zip(tokens, range(start_index, num_tokens + start_index)))
+    token_to_index = dict(
+        zip(tokens, range(start_index, num_tokens + start_index)))
     return token_to_index
 
+
 def index_with_special_tokens(tokens,
-    with_pad=True, with_bos=True, with_eos=True, with_unk=True, with_overflow=True,
-    pad_token="_PAD_", bos_token="_BOS_", eos_token="_EOS_",
-    unk_token="_UNK_", overflow_token="_OVERFLOW_"):
+                              with_pad=True,
+                              with_bos=True,
+                              with_eos=True,
+                              with_unk=True,
+                              with_overflow=True,
+                              pad_token="_PAD_",
+                              bos_token="_BOS_",
+                              eos_token="_EOS_",
+                              unk_token="_UNK_",
+                              overflow_token="_OVERFLOW_"):
 
     num_normal_tokens = len(tokens)
-    num_special_tokens = sum([with_pad, with_bos, with_eos, with_unk, with_overflow])
-    token_to_index = dict(zip(tokens, range(num_special_tokens, num_normal_tokens + num_special_tokens)))
+    num_special_tokens = sum(
+        [with_pad, with_bos, with_eos, with_unk, with_overflow])
+    token_to_index = dict(
+        zip(tokens,
+            range(num_special_tokens, num_normal_tokens + num_special_tokens)))
 
     idx = 0
     if with_pad:
@@ -61,10 +100,17 @@ def index_with_special_tokens(tokens,
         idx += 1
     return token_to_index
 
-def tokenize(sequence, token_to_index, target_length,
-        unk_token='_UNK_', bos_token="_BOS_", eos_token="_EOS_",
-        pad_token="_PAD_", overflow_token="_OVERFLOW_",
-        num_bos=1, num_eos=1):
+
+def tokenize(sequence,
+             token_to_index,
+             target_length,
+             unk_token='_UNK_',
+             bos_token="_BOS_",
+             eos_token="_EOS_",
+             pad_token="_PAD_",
+             overflow_token="_OVERFLOW_",
+             num_bos=1,
+             num_eos=1):
 
     out_seq = []
     if num_bos > 0:
@@ -72,7 +118,8 @@ def tokenize(sequence, token_to_index, target_length,
         out_seq.extend([bos_idx] * num_bos)
 
     for tk in sequence:
-        tk_idx = token_to_index[tk] if tk in token_to_index else token_to_index[unk_token]
+        tk_idx = token_to_index[tk] if tk in token_to_index else token_to_index[
+            unk_token]
         out_seq.append(tk_idx)
 
     if num_eos > 0:
@@ -89,23 +136,53 @@ def tokenize(sequence, token_to_index, target_length,
         out_seq = out_seq[:target_length]
     return out_seq
 
+
 def untokenize(sequence, index_to_token):
     return [index_to_token[idx] for idx in sequence]
 
-def character_tokenize(sentence, char_to_index, target_length,
-        unk_token='_UNK_', bos_token="_BOS_", eos_token="_EOS_", pad_token="_PAD_",
-        num_bos=1, num_eos=1):
-    return tokenize(sentence, char_to_index, target_length,
-        unk_token=unk_token, bos_token=bos_token, eos_token=eos_token, pad_token=pad_token,
-        num_bos=num_bos, num_eos=num_eos)
 
-def word_tokenize(sentence, word_to_index, target_length,
-        unk_token='_UNK_', bos_token="_BOS_", eos_token="_EOS_", pad_token="_PAD_",
-        num_bos=1, num_eos=1):
+def character_tokenize(sentence,
+                       char_to_index,
+                       target_length,
+                       unk_token='_UNK_',
+                       bos_token="_BOS_",
+                       eos_token="_EOS_",
+                       pad_token="_PAD_",
+                       num_bos=1,
+                       num_eos=1):
+    return tokenize(
+        sentence,
+        char_to_index,
+        target_length,
+        unk_token=unk_token,
+        bos_token=bos_token,
+        eos_token=eos_token,
+        pad_token=pad_token,
+        num_bos=num_bos,
+        num_eos=num_eos)
+
+
+def word_tokenize(sentence,
+                  word_to_index,
+                  target_length,
+                  unk_token='_UNK_',
+                  bos_token="_BOS_",
+                  eos_token="_EOS_",
+                  pad_token="_PAD_",
+                  num_bos=1,
+                  num_eos=1):
     tokens = split_sentence_into_word_tokens(sentence)
-    return tokenize(tokens, word_to_index, target_length,
-        unk_token=unk_token, bos_token=bos_token, eos_token=eos_token, pad_token=pad_token,
-        num_bos=num_bos, num_eos=num_eos)
+    return tokenize(
+        tokens,
+        word_to_index,
+        target_length,
+        unk_token=unk_token,
+        bos_token=bos_token,
+        eos_token=eos_token,
+        pad_token=pad_token,
+        num_bos=num_bos,
+        num_eos=num_eos)
+
 
 def zero_out_digits(s):
     out_s = []
@@ -116,30 +193,37 @@ def zero_out_digits(s):
             out_s.append(ch)
     return ''.join(out_s)
 
+
 def lowercase(s):
     return s.lower()
+
 
 def split_sentence_into_word_tokens(sentence):
     from nltk import word_tokenize as nltk_word_tokenize
     return nltk_word_tokenize(sentence)
+
 
 def convert_onehot_to_indices(y_onehot):
     assert len(y_onehot.shape) == 2
     y_idx = np.where(y_onehot > 0.0)[1]
     return y_idx
 
+
 def convert_indices_to_onehot(y_idx, num_classes):
     assert len(y_idx.shape) == 1
     num_examples = y_idx.shape[0]
     y_one_hot = np.zeros((num_examples, num_classes), dtype='float32')
-    y_one_hot[np.arange(num_examples),  y_idx] = 1.0
+    y_one_hot[np.arange(num_examples), y_idx] = 1.0
     return y_one_hot
+
 
 def all_mask(shape):
     return np.ones(shape, dtype='float32')
 
+
 def none_mask(shape):
     return np.zeros(shape, dtype='float32')
+
 
 def mask_union(mask_lst):
     out_mask = np.zeros_like(mask_lst[0], dtype='float32')
@@ -148,6 +232,7 @@ def mask_union(mask_lst):
     out_mask = np.clip(out_mask, 0.0, 1.0)
     return out_mask
 
+
 def mask_intersection(mask_lst):
     out_mask = np.ones_like(mask_lst[0], dtype='float32')
     for m in mask_lst:
@@ -155,8 +240,10 @@ def mask_intersection(mask_lst):
         out_mask *= m
     return out_mask
 
+
 def mask_invert(mask):
     return 1.0 - mask
+
 
 def mask_from_lengths(length_lst, max_length):
     n = len(length_lst)
@@ -165,21 +252,27 @@ def mask_from_lengths(length_lst, max_length):
         out_mask[i, x:] = 0.0
     return out_mask
 
+
 def mask_indices1d(mask):
     assert len(mask.shape) == 1
     return np.where(mask > 0.5)[0]
+
 
 def true_indices1d(x):
     assert len(x.shape) == 1
     return np.where(x)[0]
 
+
 def num_dims(x):
     return len(x.shape)
+
 
 def pad_tensor(x, output_shape, pad_val, left_corner_pos):
     assert len(x.shape) == len(output_shape)
     assert len(left_corner_pos) == len(output_shape)
-    assert all(in_dim + idx <= out_dim for in_dim, out_dim, idx in zip(x.shape, output_shape, left_corner_pos))
+    assert all(
+        in_dim + idx <= out_dim
+        for in_dim, out_dim, idx in zip(x.shape, output_shape, left_corner_pos))
     # NOTE: current limitation, but not a big problem in practice. improve later.
     assert len(output_shape) <= 5 and len(output_shape) > 0
     assert type(output_shape) == tuple and type(left_corner_pos) == tuple
@@ -202,12 +295,15 @@ def pad_tensor(x, output_shape, pad_val, left_corner_pos):
     elif len(x.shape) == 4:
         idx0, idx1, idx2, idx3 = left_corner_pos
         n0, n1, n2, n3 = x.shape
-        out_x[idx0:idx0 + n0, idx1:idx1 + n1, idx2:idx2 + n2, idx3:idx3 + n3] = x
+        out_x[idx0:idx0 + n0, idx1:idx1 + n1, idx2:idx2 + n2, idx3:idx3 +
+              n3] = x
     elif len(x.shape) == 5:
         idx0, idx1, idx2, idx3, idx4 = left_corner_pos
         n0, n1, n2, n3, n4 = x.shape
-        out_x[idx0:idx0 + n0, idx1:idx1 + n1, idx2:idx2 + n2, idx3:idx3 + n3, idx4:idx4 + n4] = x
+        out_x[idx0:idx0 + n0, idx1:idx1 + n1, idx2:idx2 + n2, idx3:idx3 +
+              n3, idx4:idx4 + n4] = x
     return out_x
+
 
 def reshape_apply(x, reshape_fn, apply_fn, pre_shape=None, post_shape=None):
     if pre_shape is not None:
@@ -217,11 +313,14 @@ def reshape_apply(x, reshape_fn, apply_fn, pre_shape=None, post_shape=None):
         x = reshape_fn(x, post_shape)
     return x
 
+
 def pack(xs):
     return np.concatenate(xs)
 
+
 def unpack(x):
     return np.split(x, x.shape[0])
+
 
 def multi_to_flat_indices(indices, shape):
     assert len(indices.shape) == 2
@@ -230,6 +329,7 @@ def multi_to_flat_indices(indices, shape):
     strides = np.cumprod(aux[::-1])[::-1].astype('int64')
     flat_indices = (indices * strides[None, :]).sum(axis=1)
     return flat_indices
+
 
 def flat_to_multi_indices(indices, shape):
     assert len(indices.shape) == 1
@@ -245,10 +345,12 @@ def flat_to_multi_indices(indices, shape):
         acc -= x * multi_indices[:, idx]
     return multi_indices
 
+
 def gather(x, indices):
     assert len(x.shape) == 2
     assert len(indices.shape) == 1
     return x[indices]
+
 
 # NOTE: this is a stateful update.
 def scatter_update(x, y, from_indices, to_indices):
@@ -257,6 +359,7 @@ def scatter_update(x, y, from_indices, to_indices):
     assert from_indices.shape[0] == to_indices.shape[0]
     y[to_indices] = x[from_indices]
     return y
+
 
 def sparse_column_to_multi_indices(col_indices_lst):
     num_indices = sum([len(col_idxs) for col_idxs in col_indices_lst])
@@ -268,6 +371,7 @@ def sparse_column_to_multi_indices(col_indices_lst):
         out[left:right, 1] = col_idxs
         left = right
     return out
+
 
 def sparse_column_with_row_to_multi_indices(row_indices, col_indices_lst):
     assert len(row_indices) == len(col_indices_lst)
@@ -282,6 +386,7 @@ def sparse_column_with_row_to_multi_indices(row_indices, col_indices_lst):
         left = right
     return out
 
+
 def sorting_indices(x, decreasing):
     assert len(x.shape) == 2
     indices = np.argsort(x, axis=1)
@@ -289,15 +394,18 @@ def sorting_indices(x, decreasing):
         indices = np.fliplr(indices)
     return indices
 
+
 def topk(x, k):
     indices = sorting_indices(x, True)[:, :k]
     aux_indices = np.tile(np.arange(x.shape[0])[:, None], (1, k))
     return x[aux_indices, indices]
 
+
 def bottomk(x, k):
     indices = sorting_indices(x, False)[:, :k]
     aux_indices = np.tile(np.arange(x.shape[0])[:, None], (1, k))
     return x[aux_indices, indices]
+
 
 def reflect(x, axis):
     return np.flip(x, axis)
